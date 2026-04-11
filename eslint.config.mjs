1d0d25e350de87fc5b528ade731294b6bee56853
bibekjodd <bibekjodd@gmail.com>
2026-04-11 14:41:06 +0545

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,31 @@
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTs from 'eslint-config-next/typescript';
-import { defineConfig, globalIgnores } from 'eslint/config';
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts'
-  ]),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+const eslintConfig = [
+  {
+    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts']
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-empty-object-type': 'off',
-      'react-hooks/exhaustive-deps': 'off',
-      'react/display-name': 'off',
-      '@next/next/no-img-element': 'off',
-      'jsx-a11y/alt-text': 'off'
+      'react/no-unescaped-entities': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/display-name': 'warn',
+      '@next/next/no-img-element': 'warn',
+      'jsx-a11y/alt-text': 'warn'
     }
   }
-]);
+];
 
 export default eslintConfig;
